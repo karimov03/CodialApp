d4cf51b5dcde6eb4e49b114580b016d141e4d040
karimov03 <123530135+karimov03@users.noreply.github.com>
2023-04-11 16:36:41 +0500

    Initial commit

---
+++ app/src/main/java/com/karimov03/codialapp/Class/Gruhlar.kt
@@ -0,0 +1,11 @@
+package com.karimov03.codialapp.Class
+
+class Gruhlar {
+    var id:Int?=null
+    var name:String?=null
+    var vaqt:String?=null
+    var kun:String?=null
+    var mentor_id:String?=null
+
+
+}
---
+++ app/src/main/java/com/karimov03/codialapp/Class/Mentor.kt
@@ -0,0 +1,7 @@
+package com.karimov03.codialapp.Class
+
+class Mentor {
+    var id:Int?=null
+    var name:String?=null
+
+}
